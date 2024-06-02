6a45bc0a2cd8fded5b15e9c1dbefa4e9a45a89ca
huang <1149823272@qq.com>
2024-06-02 11:58:50 +0800

    HotFix edit

--- JavaTest/src/main/java/HotFix.java
+++ JavaTest/src/main/java/HotFix.java
@@ -1,6 +1,12 @@
+import java.sql.SQLOutput;
+
 /**
  * @author huang
  * @date 2024/6/2 11:49
  */
 public class HotFix {
+
+    public static void main(String[] args) {
+        System.out.println("HotFix");
+    }
 }
